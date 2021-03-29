b05bc526758a267040361b4990f1652f6defd61d
Catarina-Carneiro <catarina.carneiro@bandtec.com.br>
2021-03-28 22:51:37 -0300

    att

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var container = document.getElementById("app")
 
 function Contador(props){
 
+    const cardStyle = {
+     color: props.cor || '#f00',
+      }
+
+
+
     
     const[numero, setNumero] = React.useState(0);
 
@@ -19,8 +25,8 @@ function Contador(props){
     }
     return(
         <React.Fragment>
-            <h1>{props.titulo}</h1>
-            <h3>{numero}</h3>
+            <h1 style={cardStyle}>{props.titulo}</h1>
+            <h3 style={cardStyle} >{numero}</h3>
             <button onClick={somar}>+</button>
             <button onClick={subtrair}>-</button>
            
@@ -32,12 +38,12 @@ function Contador(props){
 function App(){
 return(
     <React.Fragment>
-        <h1> Qual seu favorito para os games!</h1>
+        <h1 cor= "FFFFFFFF"> Qual seu favorito para os games!</h1>
         <h3></h3>
-        <Contador titulo= "nitendo"/>
-        <Contador titulo="playstation"/>
-        <Contador titulo= "xbox"/>
-        <Contador titulo="pc gamer"/>
+        <Contador titulo= "nitendo" />
+        <Contador titulo="playstation" cor="#00BFFF"/>
+        <Contador titulo= "xbox" cor="#00FF00"/>
+        <Contador titulo="pc gamer" cor="#FFFF00"/>
     </React.Fragment>
 );
 
